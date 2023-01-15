988ae3f1df8ccfc9ad7cbde374258af3293eab3d
Clovis Rosa <clovis.souzarosa@gmail.com>
2023-01-15 23:25:56 +0000

    Art Institute project start. Fetching basic data. Working on Schema.

---
+++ typing.d.ts
@@ -0,0 +1,14 @@
+type Exhibitions = {
+  date: string;
+  description: string;
+  id: string;
+  slug: string;
+  title: string;
+  content: {
+    html: string;
+  };
+  image: {
+    id: string;
+    url: string;
+  };
+};
